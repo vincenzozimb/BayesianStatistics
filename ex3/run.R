aa0ba7afab2b8bf9d23165ab98a671646a876273
Vincenzo <vzimbardo9@gmail.com>
2023-06-07 10:24:01 +0200

    ex3 created

---
+++ ex3/run.R
@@ -0,0 +1,10 @@
+## Type this command in the R-terminal before working in this folder 
+# setwd("/home/vincenzo/Desktop/University/BayesianStatistics/ex3")
+
+# Now you are ready 
+library(rmarkdown)
+render('VincenzoZimbardo-bs-3.Rmd', 'html_document')
+system("gnome-terminal -- google-chrome VincenzoZimbardo-bs-3.html")
+
+# render('ElisaMuratore-bs-3.Rmd', 'html_document')
+# system("gnome-terminal -- google-chrome ElisaMuratore-bs-3.html")
