02a1370bb381c8b35b3da78c20b4a77b2910f902
Vincenzo <vzimbardo9@gmail.com>
2023-07-03 10:37:46 +0200

    homework4 started

---
+++ ex4/run.R
@@ -0,0 +1,8 @@
+## Type this command in the R-terminal before working in this folder 
+# setwd("/home/vincenzo/Desktop/University/BayesianStatistics/ex4")
+
+# Now you are ready 
+library(rmarkdown)
+
+render('VincenzoZimbardo-bs-4.Rmd', 'html_document')
+system("gnome-terminal -- google-chrome VincenzoZimbardo-bs-4.html")
